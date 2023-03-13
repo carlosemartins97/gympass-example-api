be7e172ecd67ce974a0ee910085b8bd3831303f9
Carlos Martins <carlosemartins97@gmail.com>
2023-03-12 23:15:25 -0300

    feat: get user profile

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -5,6 +5,16 @@ export class InMemoryUsersRepository implements UsersRepository {
 
   public items: User[] = [];
 
+  async findById(id: string) {
+    const user = this.items.find(item => item.id === id);
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
+
   async create(data: Prisma.UserCreateInput) {
     const user = {
       id: '1',
---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,57 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { compare, hash } from 'bcryptjs'
+import { beforeEach } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { AuthenticateUseCase } from './authenticate'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { GetUserProfileUseCase } from './get-user-profile'
+
+
+let userRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Authenticate Use Case', () => {
+
+  beforeEach(() => {
+    userRepository = new InMemoryUsersRepository();
+    sut = new GetUserProfileUseCase(userRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const email = 'jhon@teste.com'
+    const password = '123456'
+
+    const createdUser = await userRepository.create({
+      name: 'John',
+      email,
+      password_hash: await hash(password, 6)
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id
+    })
+
+    expect(user.id).toEqual(expect.any(String));
+    expect(user.name).toEqual('John')
+  })
+
+  it('should not be able to get user profile with wrong id', async () => {
+    const email = 'jhon@teste.com'
+    const password = '123456'
+
+    const createdUser = await userRepository.create({
+      name: 'John',
+      email,
+      password_hash: await hash(password, 6)
+    })
+
+
+
+    await expect(async () => {
+      const { user } = await sut.execute({
+        userId: 'non-existing-id'
+      })
+    }).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+})
