e8b76ea834ac4da1a67c02cf5d2b52b24ae6ca22
Carlos Martins <carlosemartins97@gmail.com>
2023-03-14 14:04:33 -0300

    feat: e2e register

---
+++ src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,10 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { describe } from 'vitest'
+import { it } from 'node:test'
+
+describe('Authenticate Controller', () => {
+  it('', () => {
+
+  })
+})
