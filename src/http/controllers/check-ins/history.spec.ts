b378601164218d1f88a6f1b2a4ec2053228f77fb
Carlos Martins <carlosemartins97@gmail.com>
2023-03-15 14:21:39 -0300

    feat: check ins e2e

---
+++ src/http/controllers/check-ins/history.spec.ts
@@ -0,0 +1,50 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
+
+describe('Check in history (e2e)', () => {
+
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to list check in history', async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Javascript Gym',
+        description: "Some description",
+        phone: '12312312',
+        latitude: -27.2092052,
+        longitude: -49.5401091,
+      }
+    })
+
+
+    await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -27.2092052,
+        longitude: -49.5401091,
+      })
+
+    const response = await request(app.server)
+      .get(`/check-ins/history`)
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.checkIns).toEqual([
+      expect.objectContaining({
+        gym_id: gym.id
+      })
+    ])
+  })
+})
